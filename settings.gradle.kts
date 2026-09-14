48d70785830082a8b3446aea9fa403d1a094ae36
gitthub999 <pongkojbarmon23@gmail.com>
2026-09-14 17:29:41 +0600

    Remove copyright notice and simplify repository declarations

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,8 @@
 import org.gradle.kotlin.dsl.withType
 
-/*
- * Copyright 2026 pyamsoft
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 pluginManagement {
   repositories {
-    maven {
-      url = uri(rootProject.projectDir.resolve("local-repo"))
-    }
+    maven { url = uri(rootProject.projectDir.resolve("local-repo")) }
     mavenLocal()
     google()
     mavenCentral()
@@ -39,14 +21,11 @@ pluginManagement {
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
-    maven {
-      url = uri("local-repo")
-    }
+    maven { url = uri("local-repo") }
     mavenLocal()
     google()
     mavenCentral()
     gradlePluginPortal()
-    // Jitpack
     maven {
       setUrl("https://jitpack.io")
       content {
@@ -71,10 +50,6 @@ gradle.lifecycle.beforeProject {
     reports.junitXml.required.set(false)
     maxHeapSize = "4g"
   }
-}
-
-plugins {
-  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" apply true
 }
 
 rootProject.name = "TetherFuseNet"
